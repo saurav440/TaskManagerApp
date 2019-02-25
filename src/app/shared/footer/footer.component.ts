8561549297e2569ddd719a258ad39e2ae7821eac
saurav440 <sauravkumar40@gmail.com>
2019-02-25 22:25:54 +0530

    added current year in footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -7,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 currentYear:any;
+
   constructor() { }
 
   ngOnInit() {
-    this.currentYear= "2019";
+    this.currentYear= new Date().getFullYear();
   }
 
 }
