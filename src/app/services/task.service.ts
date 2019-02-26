2f332f75aa0a9f72a6628036bf0df5967cbe2774
saurav440 <sauravkumar40@gmail.com>
2019-02-26 12:06:05 +0530

    add comments and correct the delete tals service url

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-
 import { map } from 'rxjs/Operators';
 
 import { TaskDetail } from 'src/app/models/task-detail';
@@ -15,15 +14,16 @@ export class TaskService {
   constructor(private http: Http) { }
   baseUrl: string = 'http://localhost/TaskManagerService/api/';
 
+  // Get the all task list
   getTasks() {
     return this.http.get(this.baseUrl + 'GetAll')
       .pipe(map((response) => {
         return response.json();
       })
       );
-
   }
 
+//create new task 
   addTask(task: TaskDetail) {
     return this.http.post(this.baseUrl + 'AddTask', task)
       .pipe(map((response) => {
@@ -31,6 +31,7 @@ export class TaskService {
       })
       );
   }
+  //get task detail by task id
   getTaskById(id) {
     return this.http.get(this.baseUrl + 'GetTask/' + id)
       .pipe(map((response) => {
@@ -39,7 +40,7 @@ export class TaskService {
       })
       );
   }
-
+// update the task details
   updateTask(task: TaskDetail) {
     return this.http.put(this.baseUrl + '/UpdateTask', task)
       .pipe(map((response) => {
@@ -47,21 +48,23 @@ export class TaskService {
       })
       );
   }
+  //end the task 
   endTask(id: string) {
     return this.http.get(this.baseUrl + 'EndTask/' + id)
-    .pipe(map((response) => {
-      console.log(response);
-      return response;
-    })
-    );
+      .pipe(map((response) => {
+        console.log(response);
+        return response;
+      })
+      );
   }
+  //delete the task
   deleteTask(id: string) {
-    return this.http.delete(this.baseUrl + 'EndTask/' + id)
-    .pipe(map((response) => {
-      console.log(response)
-      return response;
-    })
-    );
+    return this.http.delete(this.baseUrl + 'DeleteTask/' + id)
+      .pipe(map((response) => {
+        console.log(response)
+        return response;
+      })
+      );
   }
 
 }
